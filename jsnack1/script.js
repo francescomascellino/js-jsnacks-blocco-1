e2cada9e9ba0a6a01edbd847a1f31f1bd9dd9384
francescomascellino <franesco.mascellino@gmail.com>
2023-08-29 14:33:24 +0200

    snack 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,26 @@
 /* L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
+const firstNum = Number(prompt("Inserisci un numero", "1"));
+
+const secondNum = Number(prompt("Inserisci un numero", "2"));
+
+const resulEl = document.querySelector("h1");
+
+if (firstNum > secondNum) {
+
+    resulEl.innerHTML = `${firstNum} è il numero maggiore`
+
+} else if (secondNum > firstNum) {
+
+    resulEl.innerHTML = `${secondNum} è il numero maggiore`
+
+} else if (isNaN(firstNum) || isNaN(secondNum)) {
+
+    resulEl.innerHTML = "Uno dei valori inseriti non è valido"
+
+} else {
+
+    resulEl.innerHTML = `${firstNum} e ${secondNum} sono uguali`
+
+}
