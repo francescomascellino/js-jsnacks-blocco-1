4861ebade0be7b0c152355f10ed513703d03d341
francescomascellino <franesco.mascellino@gmail.com>
2023-08-29 15:52:45 +0200

    snack bonus

--- jsnackbonus/script.js
+++ jsnackbonus/script.js
@@ -2,3 +2,25 @@
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
 
+const numArray = [];
+
+const numList = document.querySelector("h1");
+
+for (let i = 0; i < 6; i++) {
+    const userValue = prompt("inserisci un numero", `${i +  1}`);
+
+    if (isNaN(userValue)) {
+
+        numList.innerHTML = "Uno dei valori inseriti non è un numero";
+
+    } else if (userValue % 2 !== 0) {
+
+    numArray.push(userValue);
+
+    }
+
+}
+
+console.log(numArray);
+
+numList.innerHTML = `Ecco l'elenco dei valori dispari inseriti: ${numArray.join(", ")}.`;
