3aa52e6e161274cd622d024d715677a0758b2256
francescomascellino <franesco.mascellino@gmail.com>
2023-08-29 15:02:01 +0200

    snack 2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,3 +1,24 @@
 /* L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga. */
 
+//.split(" ") separa la stringa in un array ogni volta che viene incontrato uno spazio. la variabile diventa quindi momentaneamente un array.
+//.join("") unisce un array in una stringa, usando come "separatore" "", ovvero nessuno spazio, quindi effettivamente riunendo le possibili parole multiple digitate dall'utemte una singola parola.
+const firstword = prompt("Inserisci del testo (gli spazi verranno eliminati)", "Cane").split(" ").join("");
+
+const secondword = prompt("Inserisci altro testo (gli spazi verranno eliminati)", "Gatto").split(" ").join("");
+
+const resulEl = document.querySelector("h1");
+
+if (firstword.length > secondword.length) {
+
+    resulEl.innerHTML = `${firstword} è la parola più lunga, ${secondword} la più corta`;
+
+} else if (secondword.length > firstword.length) {
+
+    resulEl.innerHTML = `${secondword} è la parola più lunga, ${firstword} la più corta`;
+
+} else {
+
+    resulEl.innerHTML = `${firstword} e ${secondword} hanno la stessa lunghezza`;
+
+}
